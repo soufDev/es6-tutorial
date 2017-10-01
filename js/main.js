24471adc3bc54f9a12f047b5671fd23fb77c3225
soufDev <ait.akkache.soufiane@gmail.com>
2017-10-01 16:06:54 +0100

    from function to class with ES6

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
-import {calculateAmortization} from './mortage'
+import Mortgage from './mortage'
 
 document.getElementById('calcBtn').addEventListener('click', () => {
     let principal = document.getElementById("principal").value;
     let years = document.getElementById("years").value;
     let rate = document.getElementById("rate").value;
-    let {monthlyPayment, monthlyRate, amortization} = calculateAmortization(principal, years, rate);
-    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
-    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
-    amortization.forEach(month => console.log(month))
+    let mortgage=  new Mortgage(principal, years, rate);
+
+    document.getElementById("monthlyPayment").innerHTML = mortgage.monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRate").innerHTML = (rate / 12).toFixed(2);
+    let html=""
+    mortgage.amortization.forEach((year, index) => html += `
+        <tr>
+            <td>${index + 1}</td>
+            <td class="currency">${Math.round(year.principalY)}</td>
+            <td class="stretch">
+                <div class="flex">
+                    <div class="bar principal"
+                         style="flex:${year.principalY};-webkit-flex:${year.principalY}">
+                    </div>
+                    <div class="bar interest"
+                         style="flex:${year.interestY};-webkit-flex:${year.interestY}">
+                    </div>
+                </div>
+            </td>
+            <td class="currency left">${Math.round(year.interestY)}</td>
+            <td class="currency">${Math.round(year.balance)}</td>
+        </tr>
+    `
+    )
+    document.getElementById("amortizations").innerHTML = html
 });
 
 
--- js/mortage.js
+++ js/mortage.js
@@ -1,28 +1,32 @@
-
-export let calculateMonthlyPayment = (principal, years, rate) => {
-    let monthlyRate = 0
-    if (rate) {
-        monthlyRate = rate / 100 / 12
+export default class Mortage {
+    constructor(principal, years, rate) {
+        this.principal = principal
+        this.years = years
+        this.rate = rate
     }
-    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)))
-    return {principal, years, rate, monthlyPayment, monthlyRate}
-}
 
-export let calculateAmortization = (principal, years, rate) => {
-    let {monthlyRate, monthlyPayment} = calculateMonthlyPayment(principal, years, rate)
-    let balance = principal
-    let amortization = []
-    for(let y=0; y<years ; y++) {
-        let interestY = 0
-        let principalY = 0
-        for(let m=0; m<12 ; m++){
-            let interestM = balance * monthlyRate
-            let principalM = monthlyPayment - interestM
-            interestY +=  interestM
-            principalY +=  principalM
-            balance -=  principalM
+    get monthlyPayment() {
+        let monthlyRate = this.rate / 100 / 12
+        return this.principal * monthlyRate / (1 - (Math.pow( 1 / ( 1+monthlyRate ), this.years * 12 ) ) )
+    }
+
+    get amortization() {
+        let monthlyPayment = this.monthlyPayment
+        let monthlyRate = this.rate / 100 / 12
+        let balance = this.principal
+        let amortization = []
+        for(let y=0; y<this.years ; y++) {
+            let interestY = 0
+            let principalY = 0
+            for(let m=0; m<12 ; m++){
+                let interestM = balance * monthlyRate
+                let principalM = monthlyPayment - interestM
+                interestY +=  interestM
+                principalY +=  principalM
+                balance -=  principalM
+            }
+            amortization.push({principalY, interestY, balance})
         }
-        amortization.push({principalY, interestY, balance})
+        return amortization
     }
-    return {monthlyPayment, monthlyRate, amortization}
 }
